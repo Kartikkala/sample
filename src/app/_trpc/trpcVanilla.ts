e734fe987f27222cbc6f9e5e6f48af19a49ae04c
Kartikkala <kartikkala10december@gmail.com>
2025-06-08 20:43:03 +0530

    Updated urls from absolute to relative

--- src/app/_trpc/trpcVanilla.ts
+++ src/app/_trpc/trpcVanilla.ts
@@ -5,7 +5,7 @@ import type { AppRouter } from "@/server/trpc"; // adjust the path
 export const trpc = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: 'http://localhost:3000/api/trpc', // Update this if you're on a different port or domain
+      url: '/api/trpc', // Update this if you're on a different port or domain
     }),
   ],
 });
