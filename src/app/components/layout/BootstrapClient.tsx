2d525942deba54d2da0ee5da79a6f5f5aed65438
Kartikkala <kartikkala10december@gmail.com>
2025-06-08 21:15:21 +0530

    Imported bootstrap js dynamically

--- src/app/components/layout/BootstrapClient.tsx
+++ src/app/components/layout/BootstrapClient.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect } from 'react';
 
 export default function BootstrapClient() {
   useEffect(() => {
+    import('bootstrap/dist/js/bootstrap.bundle.min.js');
   }, []);
 
   return null;
