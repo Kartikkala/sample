d2f43b09900f241769b31fdf6e8f27dc3d27c090
Kartikkala <kartikkala10december@gmail.com>
2025-06-08 19:34:05 +0530

    Disabled dev indicators and cleared README

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  transpilePackages: ['bootstrap']
+  transpilePackages: ['bootstrap'],
+  devIndicators: false,
 };
 
 export default nextConfig;
